3d1eedad7c78cc36d2a840d9c135816c8b956362
yzm <13884935@qq.com>
2024-03-30 12:17:53 +0900

    5

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,28 @@
+"use client";
 
-"use client"
+import React, { useEffect, useRef, FC } from 'react';
 
-import React, { useEffect, useRef, FC, memo } from 'react';
+interface TradingViewWidgetProps {
+  symbol: string;
+  locale?: string;
+  interval?: string;
+  theme?: string;
+  style?: string;
+  allow_symbol_change?: boolean;
+  save_image?: boolean;
+  hide_volume?: boolean;
+}
 
-const TradingViewWidget: FC = () => {
+const CustomTradingViewWidget: FC<TradingViewWidgetProps> = ({
+  symbol,
+  locale = "zh_CN",
+  interval = "D",
+  theme = "light",
+  style = "1",
+  allow_symbol_change = true,
+  save_image = false,
+  hide_volume = true
+}) => {
   const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -13,26 +32,24 @@ const TradingViewWidget: FC = () => {
       script.type = 'text/javascript';
       script.async = true;
       script.innerHTML = JSON.stringify({
-        "width": "600",
-            "height": "400",
         "autosize": true,
-        "symbol": "BINANCE:FILUSDT",
-        "interval": "D",
+        symbol,
+        interval,
         "timezone": "Etc/UTC",
-        "theme": "light",
-        "style": "1",
-        "locale": "zh_CN",
+        theme,
+        style,
+        locale,
         "enable_publishing": false,
         "withdateranges": true,
-        "allow_symbol_change": true,
-        "save_image": false,
+        allow_symbol_change,
+        save_image,
         "calendar": false,
-        "hide_volume": true,
+        hide_volume,
         "support_host": "https://www.tradingview.com"
       });
       container.current.appendChild(script);
     }
-  }, []);
+  }, [symbol, interval, theme, style, locale, allow_symbol_change, save_image, hide_volume]);
 
   return (
     <div className="tradingview-widget-container" ref={container} style={{ height: '100%', width: '100%' }}>
@@ -46,158 +63,21 @@ const TradingViewWidget: FC = () => {
   );
 };
 
-
-
-function TradingViewWidget_zep() {
-    const container = useRef();
-  
-    useEffect(
-      () => {
-        const script = document.createElement("script");
-        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-        script.type = "text/javascript";
-        script.async = true;
-        script.innerHTML = `
-          {
-            "width": "600",
-            "height": "400",
-            "autosize": true,
-            "symbol": "MEXC:ZEPHUSDT",
-            "interval": "D",
-            "timezone": "Etc/UTC",
-            "theme": "light",
-            "style": "1",
-            "locale": "zh_CN",
-            "enable_publishing": false,
-            "withdateranges": true,
-            "allow_symbol_change": true,
-            "save_image": false,
-            "calendar": false,
-            "hide_volume": true,
-            "support_host": "https://www.tradingview.com"
-          }`;
-        container.current.appendChild(script);
-      },
-      []
-    );
-  
-    return (
-      <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
-        <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
-        <div className="tradingview-widget-copyright"><a href="https://cn.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">在TradingView上跟踪所有市场</span></a></div>
-      </div>
-    );
-  }
-
-
-
-  
-  
-
-
-
-  function TradingViewWidget_xmr() {
-    const container = useRef();
-  
-    useEffect(
-      () => {
-        const script = document.createElement("script");
-        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-        script.type = "text/javascript";
-        script.async = true;
-        script.innerHTML = `
-          {
-            "width": "600",
-            "height": "400",
-            "symbol": "KRAKEN:XMRUSD",
-            "interval": "D",
-            "timezone": "Etc/UTC",
-            "theme": "light",
-            "style": "1",
-            "locale": "zh_CN",
-            "enable_publishing": false,
-            "gridColor": "rgba(0, 0, 0, 0.06)",
-            "hide_legend": true,
-            "withdateranges": true,
-            "allow_symbol_change": true,
-            "save_image": false,
-            "calendar": false,
-            "hide_volume": true,
-            "support_host": "https://www.tradingview.com"
-          }`;
-        container.current.appendChild(script);
-      },
-      []
-    );
-  
-    return (
-      <div className="tradingview-widget-container" ref={container}>
-        <div className="tradingview-widget-container__widget"></div>
-        <div className="tradingview-widget-copyright"><a href="https://cn.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">在TradingView上跟踪所有市场</span></a></div>
-      </div>
-    );
-  }
-
-  
-
-  function TradingViewWidget_epk() {
-    const container = useRef();
-  
-    useEffect(
-      () => {
-        const script = document.createElement("script");
-        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-        script.type = "text/javascript";
-        script.async = true;
-        script.innerHTML = `
-          {
-            "width": "600",
-            "height": "400",
-            "symbol": "GATEIO:AIEPKUSDT",
-            "interval": "D",
-            "timezone": "Etc/UTC",
-            "theme": "light",
-            "style": "1",
-            "locale": "zh_CN",
-            "enable_publishing": false,
-            "gridColor": "rgba(0, 0, 0, 0.06)",
-            "hide_legend": true,
-            "withdateranges": true,
-            "allow_symbol_change": true,
-            "save_image": false,
-            "calendar": false,
-            "hide_volume": true,
-            "support_host": "https://www.tradingview.com"
-          }`;
-        container.current.appendChild(script);
-      },
-      []
-    );
-  
-    return (
-      <div className="tradingview-widget-container" ref={container}>
-        <div className="tradingview-widget-container__widget"></div>
-        <div className="tradingview-widget-copyright"><a href="https://cn.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">在TradingView上跟踪所有市场</span></a></div>
-      </div>
-    );
-  }
-
-export default function page() {
+export default function Page() {
   return (
     <div className="grid grid-cols-2 gap-4">
-    <div>
-      <TradingViewWidget />
+      <div>
+        <CustomTradingViewWidget symbol="BINANCE:FILUSDT" />
+      </div>
+      <div>
+        <CustomTradingViewWidget symbol="MEXC:ZEPHUSDT" />
+      </div>
+      <div>
+        <CustomTradingViewWidget symbol="KRAKEN:XMRUSD" />
+      </div>
+      <div>
+        <CustomTradingViewWidget symbol="GATEIO:AIEPKUSDT" />
+      </div>
     </div>
-    <div>
-      <TradingViewWidget_zep />
-    </div>
-    <div>
-      <TradingViewWidget_xmr />
-    </div>
-    <div>
-      <TradingViewWidget_epk />
-    </div>
-  </div>
-    
-  )
+  );
 }
