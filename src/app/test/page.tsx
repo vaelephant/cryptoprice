d879c22f4eca6dafb472d2a8fbcdf33ec326490b
yzm <13884935@qq.com>
2024-03-30 11:59:50 +0900

    1

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -91,6 +91,11 @@ function TradingViewWidget_zep() {
 
 
 
+  
+  
+
+
+
   function TradingViewWidget_xmr() {
     const container = useRef();
   
@@ -132,14 +137,67 @@ function TradingViewWidget_zep() {
       </div>
     );
   }
+
+  
+
+  function TradingViewWidget_epk() {
+    const container = useRef();
+  
+    useEffect(
+      () => {
+        const script = document.createElement("script");
+        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+        script.type = "text/javascript";
+        script.async = true;
+        script.innerHTML = `
+          {
+            "width": "600",
+            "height": "400",
+            "symbol": "GATEIO:AIEPKUSDT",
+            "interval": "D",
+            "timezone": "Etc/UTC",
+            "theme": "light",
+            "style": "1",
+            "locale": "zh_CN",
+            "enable_publishing": false,
+            "gridColor": "rgba(0, 0, 0, 0.06)",
+            "hide_legend": true,
+            "withdateranges": true,
+            "allow_symbol_change": true,
+            "save_image": false,
+            "calendar": false,
+            "hide_volume": true,
+            "support_host": "https://www.tradingview.com"
+          }`;
+        container.current.appendChild(script);
+      },
+      []
+    );
+  
+    return (
+      <div className="tradingview-widget-container" ref={container}>
+        <div className="tradingview-widget-container__widget"></div>
+        <div className="tradingview-widget-copyright"><a href="https://cn.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">在TradingView上跟踪所有市场</span></a></div>
+      </div>
+    );
+  }
+
 export default function page() {
   return (
+    <div className="grid grid-cols-2 gap-4">
     <div>
-        <TradingViewWidget />
-        <TradingViewWidget_zep />
-        <TradingViewWidget_xmr />
-
+      <TradingViewWidget />
     </div>
+    <div>
+      <TradingViewWidget_zep />
+    </div>
+    <div>
+      <TradingViewWidget_xmr />
+    </div>
+    <div>
+      <TradingViewWidget_epk />
+    </div>
+  </div>
     
   )
 }
